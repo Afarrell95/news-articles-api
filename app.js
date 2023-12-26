bf1bdc5b5db7fe1d43074cc0be4c2ca1fbec350d
Allison <farrella95@gmail.com>
2023-12-26 17:00:06 -0500

    getting routes and api fetch set up

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+async function getArticles() {
+  const response = await fetch(
+    "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=iYs7GVUnHNX71roBmJ9Owq79ZAqWwAGC"
+  );
+  const articles = await response.json();
+  console.log(articles);
+}
 
+getArticles();
